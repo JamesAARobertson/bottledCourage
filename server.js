c89111776574ecd986e332199db22030a6f5e753
dredwerkz <60428436+dredwerkz@users.noreply.github.com>
2023-10-25 17:07:14 +0100

    Moved cors middleware to app.js (the CORRECT file)

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 import app from "./app.js";
-import cors from "cors";
+
 const PORT = process.env.PORT ?? 3000;
 
-app.use(cors({
-        origin: '*'
-    }));
+
 
 app.listen(PORT, function () {
     console.log(`Server is listening on http://localhost:${PORT}`);
