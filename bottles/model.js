60fea7f49ac609f74fa79e4a49e6ad570e4d97f5
James Robertson <jaarobertson92@gmail.com>
2023-10-25 12:30:43 +0100

    random bottles function

--- bottles/model.js
+++ bottles/model.js
@@ -7,7 +7,7 @@ export async function getAllBottles() {
     return bottles;
 }
 
-console.log(await getAllBottles())
+// console.log(await getAllBottles())
 
 export async function createBottle(newBottle) {
     const sqlQuery = "INSERT INTO bottles (message, timestamp) VALUES ($1, $2) RETURNING *;";
@@ -17,5 +17,22 @@ export async function createBottle(newBottle) {
     return createdBottle;
 }
 
+export async function deleteBottle() {
+    const sqlQuery = "DELETE FROM bottles WHERE id = $1 RETURNING *;";
+    const paramValues = [bottleId]
+    const result = await pool.query(sqlQuery, paramValues);
+    const deleted = result.rows[0]
+    return deleted;
+}
 
+
+// function to get 3 random bottles for main feed
+export async function getRandomBottles() {
+    const sqlQuery = "SELECT * FROM bottles ORDER BY RANDOM() LIMIT 3;";
+    const result = await pool.query(sqlQuery)
+    const bottles = result.rows
+    return bottles
+}
+
+console.log(await getRandomBottles())
 
