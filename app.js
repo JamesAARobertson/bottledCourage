44ad745f003d7017b93f8e72271023b78f63ab86
dredwerkz <60428436+dredwerkz@users.noreply.github.com>
2023-10-25 15:15:46 +0100

    Commented out express static in app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.static("public"));
+// app.use(express.static("public"));
 app.use("/", bottlesRouter);
 
 export default app;
