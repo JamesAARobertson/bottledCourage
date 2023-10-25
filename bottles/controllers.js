90ed4ded9938df2ea6f05033f2126f9ce8c0afdf
Selam Ararsa <selamkedir@icloud.com>
2023-10-25 15:47:58 +0100

    cors origin allowed

--- bottles/controllers.js
+++ bottles/controllers.js
@@ -3,7 +3,7 @@ import * as bottlesModel from './model.js';
 // function to get all bottles
 
 export async function getAllBottles(req, res) {
-console.log(`getAllBottles() in controllers.js has been called`)
+  console.log(`getAllBottles() in controllers.js has been called`);
   const bottles = await bottlesModel.getAllBottles();
 
   res.status(200).json({
@@ -11,31 +11,23 @@ console.log(`getAllBottles() in controllers.js has been called`)
     payload: bottles,
     // check this later
   });
-};
-
+}
 
 export async function getBottleById(req, res) {
-        const bottleId = req.params.id;
-        const retrievedBottle = await bottlesModel.getBottleById(bottleId);
-        if (retrievedBottle) {
-          res.status(200).json({
-            success: true,
-            payload: retrievedBottle,
-          });
-        } else {
-          res.status(404).json({
-            success: false,
-            error: `No bottle found with the id of ${bottleId}`,
-          });
-        }
-      };
-
-
-
-
-
-
-
+  const bottleId = req.params.id;
+  const retrievedBottle = await bottlesModel.getBottleById(bottleId);
+  if (retrievedBottle) {
+    res.status(200).json({
+      success: true,
+      payload: retrievedBottle,
+    });
+  } else {
+    res.status(404).json({
+      success: false,
+      error: `No bottle found with the id of ${bottleId}`,
+    });
+  }
+}
 
 export async function createBottle(req, res) {
   const inputUndefined = req.body.message === undefined;
@@ -57,7 +49,7 @@ export async function createBottle(req, res) {
     success: true,
     payload: createdBottle,
   });
-};
+}
 
 export async function deleteBottle(req, res) {
   const bottleId = req.params.id;
@@ -73,14 +65,14 @@ export async function deleteBottle(req, res) {
       error: `No bottle found with the id of ${bottleId}`,
     });
   }
-};
+}
 
 export async function getRandomBottles(_req, res) {
-        const randomBottles = await bottlesModel.getRandomBottles();
+  const randomBottles = await bottlesModel.getRandomBottles();
 
-        res.status(200).json({
-                success: true,
-                payload: randomBottles,
-                // check this later
-              });
-};
+  res.status(200).json({
+    success: true,
+    payload: randomBottles,
+    // check this later
+  });
+}
