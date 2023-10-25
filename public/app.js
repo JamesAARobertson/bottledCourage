4a2ab9751bbb5d16301a7a23e9d1caa1ff5db3cf
dredwerkz <60428436+dredwerkz@users.noreply.github.com>
2023-10-25 16:29:57 +0100

    Fixed CORS issue

--- public/app.js
+++ public/app.js
@@ -1,49 +1,43 @@
 // on hover over Message in a bottle, change color/highlight.
-async function getBottle(){
-const responseRequest = await fetch ("http://127.0.0.1:5501",
-{
-    headers: {
-        Accept: "application/json",
-    },
+async function getBottle() {
+    const responseRequest = await fetch("/", {
+        headers: {
+            Accept: "application/json",
+        },
+    });
+
+     if (!responseRequest.ok) {
+        console.error(`Status: ${responseRequest.status}`);
+        console.error(`Text: ${await responseRequest.text()}`);
+        console.error("Data not available");
+        return;
+    } 
+     const responseData = await responseRequest.json(); 
+    console.log(responseRequest);
+     console.log(responseData); 
 }
-
-)
-
-if (!responseRequest.ok) {
-    console.error(`Status: ${responseRequest.status}`);
-    console.error(`Text: ${await responseRequest.text()}`);
-    console.error('Data not available');
-    return;
-  }
-  const responseData = await responseRequest.json();
-  console.log(responseData)
-}
-
-
 
 getBottle();
 // get Message in a bottle button by id. On click opens up input/textbox. Open's keyboard.
 const submitButton = document.getElementById("send-message-button");
 
-submitButton.addEventListener("click", function(){
-
+submitButton.addEventListener("click", function () {
     const existingInput = document.getElementById("myTextbox");
 
-    if (!existingInput){
-
+    if (!existingInput) {
     }
 
     const inputElement = document.createElement("input");
 
     // Set attributes for the input element
-    inputElement.type = "text";    // Text input
-    inputElement.id = "bottleMessage";  // ID for reference
+    inputElement.type = "text"; // Text input
+    inputElement.id = "bottleMessage"; // ID for reference
     inputElement.name = "userInput"; // Name attribute
 
     // Add the input element to the container div
     const container = document.getElementById("textbox");
     container.appendChild(inputElement);
-})
+});
 // Text box has max character limit
 
 // add submit/send button for sending message, on click. Send button changes blue. Text box then disappears.
@@ -52,4 +46,4 @@ submitButton.addEventListener("click", function(){
 
 // randomisation of messages displayed on home screen
 
-// 
+//
--- server.js
+++ server.js
@@ -2,11 +2,11 @@ import app from './app.js';
 import cors from 'cors';
 const PORT = process.env.PORT ?? 3000;
 
-app.use(
-  cors({
-    origin: `*`,
-  })
-);
+const corsOptions = {
+        origin: "http://127.0.0.1:5501",
+      };
+
+      app.use(cors(corsOptions));
 app.listen(PORT, function () {
   console.log(`Server is listening on http://localhost:${PORT}`);
 });
