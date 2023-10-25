e2b5172c6e0cafc30b91d98a41c90b84addcf2ab
Thomas Chappell <tom.chappell.info@gmail.com>
2023-10-25 11:17:40 +0100

    frontend branch and start

---
+++ app.js
@@ -0,0 +1,10 @@
+
+// get Message in a bottle button by id
+const submitButton = document.getElementById("send-message-button");
+
+submitButton.addEventListener("click", function(){
+
+    alert("Message in a bottle clicked!")
+})
+
+
