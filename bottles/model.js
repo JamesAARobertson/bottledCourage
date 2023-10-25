d962e69900e8939d7cf03152ca7e56d090d46f22
James Robertson <jaarobertson92@gmail.com>
2023-10-25 12:17:15 +0100

    createBottle function

--- bottles/model.js
+++ bottles/model.js
@@ -1,13 +1,21 @@
 import { pool } from "../db/index.js";
 
-export async function getAllMessages() {
-    const sqlQuery = "SELECT * FROM messages ORDER BY id ASC;";
+export async function getAllBottles() {
+    const sqlQuery = "SELECT * FROM bottles ORDER BY id ASC;";
     const result = await pool.query(sqlQuery);
-    const messages = result.rows;
-    return messages;
+    const bottles = result.rows;
+    return bottles;
 }
 
-console.log(await getAllMessages())
+console.log(await getAllBottles())
+
+export async function createBottle(newBottle) {
+    const sqlQuery = "INSERT INTO bottles (message, timestamp) VALUES ($1, $2) RETURNING *;";
+    const paramValues = [newBottle.message, newBottle.timestamp];
+    const result = await pool.query(sqlQuery, paramValues);
+    const createdBottle = result.rows[0];
+    return createdBottle;
+}
 
 
 
--- db/helpers.js
+++ db/helpers.js
@@ -1,4 +1,4 @@
-import { pool } from "../server.js"
+import { pool } from "./index.js"
 
 /**
  *
@@ -7,8 +7,8 @@ import { pool } from "../server.js"
 
 export async function resetAllTables(data) {
         await pool.query(`
-        DROP TABLE IF EXISTS messages;
-        CREATE TABLE messages (
+        DROP TABLE IF EXISTS bottles;
+        CREATE TABLE bottles (
                 id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
                 message VARCHAR(255) NOT NULL,
                 timestamp TIMESTAMPTZ NOT NULL
@@ -17,12 +17,12 @@ export async function resetAllTables(data) {
 
         const inserted = await pool.query(`
 
-        INSERT INTO messages (
+        INSERT INTO bottles (
                 message,
                 timestamp
                 ) (
                         SELECT message, timestamp
-                        FROM json_populate_recordset(NULL::messages, $1::JSON)
+                        FROM json_populate_recordset(NULL::bottles, $1::JSON)
                 )
                 RETURNING *;`,
                 [JSON.stringify(data)]
