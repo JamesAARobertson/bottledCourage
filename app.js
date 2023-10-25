76d0a2bcfcdf545356a9a019a7a7788127930c51
Thomas Chappell <tom.chappell.info@gmail.com>
2023-10-25 14:16:05 +0100

    added JS pseudocode comments

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+// on hover over Message in a bottle, change color/highlight.
 
-// get Message in a bottle button by id
+
+// get Message in a bottle button by id. On click opens up input/textbox. Open's keyboard.
 const submitButton = document.getElementById("send-message-button");
 
 submitButton.addEventListener("click", function(){
@@ -7,4 +9,12 @@ submitButton.addEventListener("click", function(){
     alert("Message in a bottle clicked!")
 })
 
+// Text box has max character limit
 
+// add submit/send button for sending message, on click. Send button changes blue. Text box then disappears.
+
+// confirmation message to user that bottle has been sent. Display for 2 seconds.
+
+// randomisation of messages displayed on home screen
+
+// 
