4e7658302efc90da59fee71eb8e7b4cc04e71bdc
Selam Ararsa <selamkedir@icloud.com>
2023-10-25 15:14:53 +0100

    Cors: allowed requests from any origin

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-import app from "./app.js";
-
+import app from './app.js';
+import cors from 'cors';
 const PORT = process.env.PORT ?? 3000;
-
+app.use(
+  cors({
+    origin: `*`,
+  })
+);
 app.listen(PORT, function () {
   console.log(`Server is listening on http://localhost:${PORT}`);
 });
-
