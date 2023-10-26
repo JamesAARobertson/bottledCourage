00962614a23ec64586484674ea445f0f85ea0114
Elzbieta Szymichowska <ela.buu@gmail.com>
2023-10-26 18:33:04 +0100

    fix server

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 import { bottlesRouter } from "./bottles/router.js";
-import cors from "cors";
 
 const app = express();
-
-app.use(
-  cors({
-    origin: "*",
-  })
-);
 
 app.use(morgan("dev"));
 app.use(express.json());
