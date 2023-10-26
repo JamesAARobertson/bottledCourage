b3d21f499700395c43090b24cdc1bb5e70e7e603
James Robertson <jaarobertson92@gmail.com>
2023-10-26 11:09:08 +0100

    comment table and reset function for both bottles and comments

--- db/helpers.js
+++ db/helpers.js
@@ -1,21 +1,33 @@
-import { pool } from "./index.js"
+import { pool } from "./index.js";
 
 /**
  *
  * @param {{ message: string; timestamp: string}[]} data
  */
 
-export async function resetAllTables(data) {
-        await pool.query(`
-        DROP TABLE IF EXISTS bottles;
+export async function resetAllTables(data, commentData) {
+  await pool.query(`
+        DROP TABLE IF EXISTS bottles, comments;
         CREATE TABLE bottles (
-                id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                bottle_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
                 message VARCHAR(255) NOT NULL,
                 timestamp TIMESTAMPTZ NOT NULL
+
+                )
+                `);
+
+  await pool.query(`
+                CREATE TABLE comments (
+                        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                        comment VARCHAR(255) NOT NULL,
+                        timestamp TIMESTAMPTZ NOT NULL,
+                        bottle_id INT NOT NULL,
+                        FOREIGN KEY (bottle_id) REFERENCES bottles(bottle_id) ON DELETE CASCADE
         );
         `);
 
-        const inserted = await pool.query(`
+  const insertedBottles = await pool.query(
+    `
 
         INSERT INTO bottles (
                 message,
@@ -24,9 +36,24 @@ export async function resetAllTables(data) {
                         SELECT message, timestamp
                         FROM json_populate_recordset(NULL::bottles, $1::JSON)
                 )
-                RETURNING *;`,
-                [JSON.stringify(data)]
-        )
+                RETURNING *;
+                `, [JSON.stringify(data)]);
 
-        return inserted.rows;
+  const insertedComments = await pool.query( `
+        INSERT INTO comments (
+                comment,
+                timestamp,
+                bottle_id
+                ) (
+                        SELECT comment, timestamp, bottle_id
+                        FROM json_populate_recordset(NULL::comments, $1::JSON)
+                        )
+                        RETURNING *;`,
+    [JSON.stringify(commentData)]
+  );
+  const insertedData = {
+    bottles: insertedBottles.rows,
+    comments: insertedComments.rows,
+  };
+  return insertedData;
 }
--- db/scripts/reset-database.js
+++ db/scripts/reset-database.js
@@ -2,16 +2,22 @@ import { pool } from "../index.js";
 import { resetAllTables } from "../helpers.js";
 
 try {
-        const insertedRows = await resetAllTables([
-                {message: "I am feeling great today!", timestamp: "2023-10-25"},
-                {message: "I am loving databases!", timestamp: "2023-10-25"},
-                {message: "Front end devs are cool :)", timestamp: "2023-10-25"},
-        ]);
-        console.log("Reset table and inserted data");
-        console.log(insertedRows);
+  const insertedRows = await resetAllTables(
+    [
+      { message: "I am feeling great today!", timestamp: "2023-10-25" },
+      { message: "I am loving databases!", timestamp: "2023-10-25" },
+      { message: "Front end devs are cool :)", timestamp: "2023-10-25" },
+    ],
+    [
+      { comment: "Good for you", timestamp: "2023-10-26", bottle_id: 1 },
+      { comment: "Databases are fun", timestamp: "2023-10-26", bottle_id: 2 },
+      { comment: "Front end devs are the best", timestamp: "2023-10-26", bottle_id: 3 },
+    ]
+  );
+  console.log("Reset table and inserted data");
+  console.log(insertedRows);
 } catch (err) {
-        console.error(err);
+  console.error(err);
 } finally {
-        await pool.end();
+  await pool.end();
 }
-
